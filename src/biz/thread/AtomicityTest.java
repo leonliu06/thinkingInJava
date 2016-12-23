7ed006bb1a4b6e7fafad77d95a376469a6cf7086
leonliu06 <leonliu06@gmail.com>
2016-12-23 15:13:49 +0800

    synchronized

--- src/biz/thread/AtomicityTest.java
+++ src/biz/thread/AtomicityTest.java
@@ -5,6 +5,10 @@ import java.util.concurrent.Executors;
 
 public class AtomicityTest implements Runnable {
 	private int i = 0;
+	/*尽管return i确实是性的操作，但是缺少同步使得其数值可以在处于不稳定的中间状态时被 读取。
+	 * 除此之外，由于i也不是volatile的，因此还存在可视性问题。
+	 * getValue()和evenIntcrement()必须是synchronized的。
+	 * */
 	public int getValue(){ return i; }
 	private synchronized void evenIncrement(){ i++; i++; }
 	public void run(){
