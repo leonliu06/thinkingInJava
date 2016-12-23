49ce85afd18d95d66a1f3962665c5384a5144989
leonliu06 <leonliu06@gmail.com>
2016-12-23 16:26:25 +0800

    mutex

---
+++ src/biz/thread/MutexEvenGenerator.java
@@ -0,0 +1,23 @@
+package biz.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MutexEvenGenerator extends IntGenerator{
+	private int currentEvenValue = 0;
+	private Lock lock = new ReentrantLock();
+	public int next(){
+		lock.lock();
+		try{
+			++currentEvenValue;
+			Thread.yield(); // Cause failure faster
+			++currentEvenValue;
+			return currentEvenValue;
+		}finally{
+			lock.lock();
+		}
+	}
+	public static void main(String[] args) {
+		EvenChecker.test(new MutexEvenGenerator());
+	}
+}
