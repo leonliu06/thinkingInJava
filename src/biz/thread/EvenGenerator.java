9061f04628918f0d7017fc10953a5419288469a4
leonliu06 <leonliu06@gmail.com>
2016-12-23 09:49:50 +0800

    evenchecker

--- src/biz/thread/EvenGenerator.java
+++ src/biz/thread/EvenGenerator.java
@@ -1,52 +1,13 @@
-//package biz.thread;
+package biz.thread;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-abstract class IntGenerator {
-	private volatile boolean canceled = false;
-	public abstract int next();
-	// Allow this to be canceled:
-	public void cancel(){ canceled = true; }
-	public boolean isCanceled(){ return canceled; }
-}
-
-class EvenChecker implements Runnable {
-	private IntGenerator generator;
-	private final int id;
-	public EvenChecker(IntGenerator g, int ident){
-		generator = g;
-		id = ident;
-	}	
-	public void run(){
-		while(!generator.isCanceled()){
-			int val = generator.next();
-			if(val % 2 != 0){
-				System.out.println(val + " not even!");
-				generator.cancel(); // Cancels all EvenCheckers
-			}
-		}
-	}
-	// Test any type of IntGenerator:
-	public static void test(IntGenerator gp, int count){
-		System.out.println("Press Control-C to exit");
-		ExecutorService exec = Executors.newCachedThreadPool();
-		for(int i = 0; i < count; i++){
-			exec.execute(new EvenChecker(gp, count));
-		}
-		exec.shutdown();
-	}
-	// Default value for count:
-	public static void test(IntGenerator gp){
-		test(gp, 10);
-	}
-}
-
 public class EvenGenerator extends IntGenerator {
 	private int currentEvenValue = 0;
-	public synchronized int next(){
+	public int next(){
 		++currentEvenValue; // Danger point here!
-		Thread.yield();
+		//Thread.yield();
 		++currentEvenValue;
 		return currentEvenValue;
 	}
