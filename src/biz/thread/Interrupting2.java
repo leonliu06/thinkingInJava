c60c60e4ceb67a1aaf94bb64b9f4e73607359451
leonliu06 <leonliu06@gmail.com>
2017-01-03 21:21:54 +0800

    interupt

---
+++ src/biz/thread/Interrupting2.java
@@ -0,0 +1,15 @@
+package biz.thread;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+class BlockedMutex{
+	private Lock lock = new ReentrantLock();
+	public BlockedMutex(){
+		// Acquire it right away, to
+	}
+}
+
+public class Interrupting2 {
+
+}
